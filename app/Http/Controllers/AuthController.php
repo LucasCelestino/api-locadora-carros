b8f5bb5837f6598ca09d4f65a01ae1f57f3d8bf3
Lucas Celestino <lucascap899@gmail.com>
2022-09-07 14:05:38 -0300

    grupo de rotas protegidas e recuperando dados do usuario com base no jwt

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->all(['email', 'password']);
+
+        $token = auth('api')->attempt($credentials);
+
+        if (!$token)
+        {
+            return response()->json(['error' => 'Usuário ou senha inválidos'], 403);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function logout()
+    {
+        return 'logout';
+    }
+
+    public function refresh()
+    {
+        return 'refresh';
+    }
+
+    public function me()
+    {
+        dd(auth('api')->user());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,15 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::prefix('v1')->middleware('jwt.auth')->group(function(){
+    Route::apiResource('cliente', 'ClienteController');
+    Route::apiResource('carro', 'CarroController');
+    Route::apiResource('locacao', 'LocacaoController');
+    Route::apiResource('marca', 'MarcaController');
+    Route::apiResource('modelo', 'ModeloController');
+});
 
-Route::apiResource('cliente', 'ClienteController');
-Route::apiResource('carro', 'CarroController');
-Route::apiResource('locacao', 'LocacaoController');
-Route::apiResource('marca', 'MarcaController');
-Route::apiResource('modelo', 'ModeloController');
+Route::post('login', 'AuthController@login');
+Route::post('logout', 'AuthController@logout');
+Route::post('refresh', 'AuthController@refresh');
+Route::get('me', 'AuthController@me');
